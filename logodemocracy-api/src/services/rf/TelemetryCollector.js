70a102c5256e6471ab3f7feea35c9dafa3d6ed5a
chatbot01legal-maker <chatbot.01.legal@gmail.com>
2026-07-07 13:03:17 -0400

    Refactor record function in TelemetryCollector

--- logodemocracy-api/src/services/rf/TelemetryCollector.js
+++ logodemocracy-api/src/services/rf/TelemetryCollector.js
@@ -1,11 +1,31 @@
 module.exports = {
-  async record(lm, metadata, analogy, score) {
-    if (analogy) {
+
+ async record(lm, metadata, analogy, score){
+
+   if(!lm) return;
+
+   if(!lm.telemetry){
+      lm.telemetry = {};
+   }
+
+   if(!lm.telemetry.successful_analogies){
+      lm.telemetry.successful_analogies = [];
+   }
+
+   if(analogy){
+
       lm.telemetry.successful_analogies.push({
-        concept: metadata.concept,
-        analogy: analogy.analogy,
-        effectiveness: score
+
+         concept: metadata?.concept,
+
+         analogy: analogy.analogy,
+
+         effectiveness: score
+
       });
-    }
-  }
+
+   }
+
+ }
+
 };
