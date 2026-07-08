1d9555a660a78b698175305c83a46400a9541bd2
chatbot01legal-maker <chatbot.01.legal@gmail.com>
2026-07-08 18:50:53 -0400

    Add centralized configuration module for Rey Filósofo

---
+++ assets/js/config.js
@@ -0,0 +1,10 @@
+// assets/js/config.js
+// Configuración centralizada del módulo Rey Filósofo
+
+var CONFIG = (function() {
+  return {
+    API_ENDPOINT: '/api/reyfilosofo/process',
+    DEFAULT_PROVIDER: 'AcademiaContextProvider',
+    SESSION_STORAGE_KEY: 'reyfilosofo_sessionId'
+  };
+})();
